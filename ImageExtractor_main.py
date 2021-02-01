e2a92549231656faee1e4893650ccf5e6a8e073e
= <=>
2021-02-01 22:44:34 +0100

    Renamed main.py to ImageExtractor_main.py

--- ImageExtractor_main.py
+++ ImageExtractor_main.py
@@ -5,14 +5,14 @@ from datetime import datetime
 from BottleCapDetector.VideoToImage.VideoToImageExtractor import Video2ImageExtractor
 
 video_directory = r'''/home/cvvp/Projects/Computer Vision/DataSet/CV_VideoPackage'''
-output_directory = r'''/home/cvvp/Projects/git/BottleCapDetector/Output'''
+output_directory = r'''/home/cvvp/Projects/git/BottleCapDetector/BottleCapDetector/Output'''
 file_count = len(glob.glob(os.path.join(video_directory, '*.mp4')))
 
 tik = datetime.now()
 
 for i in range(1, file_count+1):
     input_video_path = os.path.join(video_directory, 'CV20_video_{id}.mp4'.format(id = i))
-    output_image_name = 'CV20_video_{id}.png'.format(id = i)
+    output_image_name = 'CV20_image_{id}.png'.format(id = i)
 
     video = cv.VideoCapture(input_video_path)
     if not video.isOpened(): 
@@ -24,7 +24,7 @@ for i in range(1, file_count+1):
 
     cv.imwrite(os.path.join(output_directory, output_image_name), image)
 
-print(frame_number)
+    print(str(i) + '-' + str(frame_number))
 
 tok = datetime.now()
 
